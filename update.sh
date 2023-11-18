f5294a325470acf08c5836d2881f6cfb6c4f813b
Maxi-Mega <ixam.agem@gmail.com>
2023-11-18 11:36:14 +0100

    Update dependencies

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eu
 
+OPENLAYERS_DIR="src/resources/vendor/openlayers"
 OPENLAYERS_JS_URL="https://cdn.jsdelivr.net/npm/ol@latest/dist/ol.js"
 OPENLAYERS_JS_MAP_URL="https://cdn.jsdelivr.net/npm/ol@latest/dist/ol.js.map"
 OPENLAYERS_CSS_URL="https://cdn.jsdelivr.net/npm/ol@latest/ol.css"
-OPENLAYERS_DIR="src/resources/vendor/openlayers"
 
 echo "Updating Go dependencies ..."
 go get -u ./... && go mod tidy
